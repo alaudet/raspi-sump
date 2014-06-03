d41e31a05aeedb74f1a028916f897965f1679b00
Al Audet <alaudet@linuxnorth.org>
2014-06-03 10:43:40 -0400

    sonar.py
    add decimal output to centimeters
    make script loop until killed
    other testing things added to determine margin of error

--- raspi-sump/sonar.py
+++ raspi-sump/sonar.py
@@ -1,48 +1,61 @@
 #!/usr/bin/python
 
-def reading(sensor):
-    import time
-    import RPi.GPIO as GPIO
+# raspi-sump, a sump pump monitoring system
+# Al Audet
+
+import time
+import RPi.GPIO as GPIO
+import decimal
+
+def reading():
     
     GPIO.setwarnings(False)
     
     GPIO.setmode(GPIO.BCM)
     
-    if sensor == 0:
+    run = 1
+    try:
+        while run == 1:
         
-        GPIO.setup(17,GPIO.OUT)
-        GPIO.setup(27,GPIO.IN)
-        GPIO.output(17, GPIO.LOW)
-        
-        time.sleep(0.3)
-        
-        GPIO.output(17, True)
-        
-        time.sleep(0.00001)
-        
-        GPIO.output(17, False)
+            GPIO.setup(17,GPIO.OUT)
+            GPIO.setup(27,GPIO.IN)
+            GPIO.output(17, GPIO.LOW)
+            
+            time.sleep(0.3)
+            
+            GPIO.output(17, True)
+            
+            time.sleep(0.00001)
+            
+            GPIO.output(17, False)
 
-        while GPIO.input(27) == 0:
-            signaloff = time.time()
-        
-        while GPIO.input(27) == 1:
-            signalon = time.time()
-        
-        timepassed = signalon - signaloff
-        
-        distance_cm = timepassed * 17000
-	       
-        return distance_cm
-         
-        GPIO.cleanup()
+            while GPIO.input(27) == 0:
+                signaloff = time.time()
+                
+            while GPIO.input(27) == 1:
+                signalon = time.time()
+                
+            timepassed = signalon - signaloff
+                      
+            decimal.getcontext().prec = 3
+            distance = decimal.Decimal(timepassed) * 17000
+            
+            distance_cm = distance
+            
+            if distance_cm >= 85:  # for testing only
+                print "> Higher than 85"
 
-    else:
-        print "Incorrect usonic() function varible."
+            elif distance_cm < 84 : # for testing only
+                print "< Lower than 84"
+            
+            
+            print distance_cm
+             
+            GPIO.cleanup()
+            time.sleep(10)
+    
+    except KeyboardInterrupt:
+        print "Script killed by user"
+    
 
-x = reading(0)
-print "The distance is %f" % x,
-print "centimeters"
-
-inches = x * float(0.39)
-print "The distance is %f" % inches,
-print "inches"
+reading()
