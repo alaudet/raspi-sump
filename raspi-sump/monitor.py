be465e03578a471727a67983393ee1aa62985f49
Al Audet <alaudet@linuxnorth.org>
2014-06-09 20:58:11 -0400

    Remove sys argv and replace with date timestamped csv file.
    Generate new csv file when date changes
    Add a bit of documentation in script
    check water level every 30 seconds

--- raspi-sump/monitor.py
+++ raspi-sump/monitor.py
@@ -4,20 +4,17 @@
 # Al Audet
 # http://www.linuxnorth.org/raspi-sump/
 
-from sys import argv
 import time
 import decimal
 import smtplib
 import string
 import RPi.GPIO as GPIO
 
-script, filename = argv
-
 def water_level():
-
+    """Measure the distance of water using the HC-SR04 Ultrasonic Sensor."""
     trig_pin = 17  # GPIO pin 17 connected to Trig on HC-SR04 sensor.
     echo_pin = 27  # GPIO pin 27 connected to Echo on HC-SR04 sensor. 
-    critical_distance = 30 
+    critical_distance = 79.9 
 
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
@@ -52,29 +49,33 @@ def water_level():
     
     except KeyboardInterrupt:
         print "Script killed by user"
-        target.close()
-
+        
 def handle_error(sample, critical_distance):
+    """Eliminates fringe error readings by using the median reading of a
+    sorted sample."""
     sorted_sample = sorted(sample)
     true_distance = sorted_sample[5] # median reading
+    filename = "waterlevel-%s.csv" % time.strftime("%Y%m%d")
+    capture = open(filename, 'a')
     
     if true_distance < critical_distance:
-        smtp_alerts(true_distance) 
+        smtp_alerts(true_distance, capture)
     else:
-        level_good(true_distance)   
+        level_good(true_distance, capture)   
 
-def level_good(how_far):
+def level_good(how_far, target):
+    """Process reading if level is greater then critical distance."""
     decimal.getcontext().prec = 3 
     how_far_clean = decimal.Decimal(how_far) * 1
-    
-    print how_far_clean # only while testing
-    
+    print how_far_clean
     target.write(time.strftime("%H:%M:%S,")),
     target.write(str(how_far_clean)),
     target.write("\n")
-    time.sleep(5)
-    
-def smtp_alerts(how_far): 
+    target.close()
+    time.sleep(30)
+
+def smtp_alerts(how_far, target):
+    """Process reading and generate alert if less then critical distance."""
     username = "your smtp username here "
     password = "your smtp password here"
     smtp_server = "smtp.gmail.com:587"
@@ -82,15 +83,13 @@ def smtp_alerts(how_far):
     decimal.getcontext().prec = 3 
     how_far_clean = decimal.Decimal(how_far) * 1
     
-    print how_far_clean, # only while testing
-    print "email sent" # only while testing
-    
     target.write(time.strftime("%H:%M:%S,")),
     target.write(str(how_far_clean)),
     target.write("\n")
-       
-    email_from = "sender email"
-    email_to = "recipient email or wireless carrier sms #"
+    target.close()   
+
+    email_from = "Raspi-Sump <email@yourdomain.com>"
+    email_to = "email@yourdomain.com or wireless sms email for sms alerts"
     email_body = string.join((
         "From: %s" % email_from,
         "To: %s" % email_to,
@@ -100,17 +99,12 @@ def smtp_alerts(how_far):
         how_far_clean),), "\r\n"
         )
     
-    print email_from # only while testing
-    print email_to # only while testing
-    print email_body #only while testing
-   
     server = smtplib.SMTP(smtp_server)
     server.starttls() 
     server.login(username, password) 
     server.sendmail(email_from, email_to, email_body)
     server.quit()
-    time.sleep(5)
+    time.sleep(30)
 
 if __name__ == "__main__":
-    target = open(filename, 'a')
     water_level()
