0b81ca6942fee050817756706c513a75ea2b2435
Al Audet <alaudet@linuxnorth.org>
2014-06-11 21:06:30 -0400

    Remove files that are no longer needed

--- raspi-sump/cron/README.md
+++
@@ -1,3 +0,0 @@
-Any cron jobs required will be put here.
-
-1 - cron for csv copy. This file will be processed on the web server.
--- raspi-sump/cron/move_file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ssh_key=~/.ssh/<private_key_name>
-source=path/to/dir/with/csv/files
-dest=user@host:path/to/destination/folder
-
-rsync -r -a -v -e "ssh -i $ssh_key" $source $dest
