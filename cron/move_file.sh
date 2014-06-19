d1a19238933c7ad56835d5fba8be6c69bb82bb24
Al Audet <alaudet@linuxnorth.org>
2014-06-18 21:23:47 -0400

    Correction to move_file.sh

--- cron/move_file.sh
+++ cron/move_file.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-ssh_key=~/.ssh/<private_key_name>
-source=/home/pi/raspi-sump/csv/
+source=/home/pi/raspi-suimp/csv/*
 dest=user@host:path/to/destination/folder
 
-rsync -r -a -v -e "ssh -i $ssh_key" $source $dest
+rsync -r -a -v -e "ssh -i /home/pi/.ssh/<identity file>" $source $dest
