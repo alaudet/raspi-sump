a032f21b0e3382be5aeaa4c6983f5747840265bf
Al Audet <alaudet@linuxnorth.org>
2014-06-18 21:27:20 -0400

    typo in move_file.sh

--- cron/move_file.sh
+++ cron/move_file.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source=/home/pi/raspi-suimp/csv/*
+source=/home/pi/raspi-sump/csv/*
 dest=user@host:path/to/destination/folder
 
 rsync -r -a -v -e "ssh -i /home/pi/.ssh/<identity file>" $source $dest
