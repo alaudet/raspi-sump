3e94552e23ce94bbda6c0a50ca49c0bdb8e97d57
Al Audet <alaudet@linuxnorth.org>
2014-07-03 20:13:43 -0400

    Change code in todaychart.py to make graph nicer looking

--- raspisump/todaychart.py
+++ raspisump/todaychart.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python
 
-# Raspi-sump, a sump pump monitoring system.
-# Al Audet
-# http://www.linuxnorth.org/raspi-sump/
-
 import time
 import numpy as np
 import matplotlib as mpl
@@ -13,23 +9,25 @@ import matplotlib.dates as mdates
 from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 
+csvfile = "/home/pi/raspi-sump/csv/waterlevel-%s.csv" % time.strftime('%Y%m%d')
 filename = "/home/pi/raspi-sump/charts/today.png" 
 
-def graph(filename):
-    csvfile = "/home/pi/raspi-sump/csv/waterlevel-%s.csv" % time.strftime('%Y%m%d')
+def graph(csvfile, filename):
     date, value = np.loadtxt(csvfile, delimiter=',', unpack=True,
             converters = {0:mdates.strpdate2num('%H:%M:%S')})
-
-    fig = plt.figure(figsize=(8,3))        
-    ax1 = fig.add_subplot(111, axisbg='white')
-
-    plt.plot_date(x=date, y=value, ls='solid', color='green', fmt=':')
+    fig = plt.figure(figsize=(10,3.5))        
+    ax1 = fig.add_subplot(111, axisbg='white', frameon=False)
+    rcParams.update({'font.size': 9})
+    plt.plot_date(x=date, y=value, ls='solid', linewidth=2, color='#FB921D', fmt=':')
     title = "Sump Pit Water Level %s" % time.strftime('%Y-%m-%d %H:%M')
-    plt.title(title)
-    plt.ylabel('Water Level (centimeters)')
+    t = plt.title(title)
+    t.set_y(1.09)
+    plt.subplots_adjust(top=0.86)
+    plt.ylabel('Centimeters')
     plt.xlabel('Time of Day')
     plt.xticks(rotation=30)    
-    plt.grid(True)
+    plt.grid(True, color='#ECE5DE', linestyle='solid')
+    plt.tick_params(axis='x', bottom='off', top='off')
     plt.savefig(filename,dpi=72)
 
-graph(filename)
+graph(csvfile, filename)
