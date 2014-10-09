88a52a0290e635292ea1b56d7fb9b3e6f7eeda7f
Al Audet <alaudet@linuxnorth.org>
2014-10-09 17:26:58 -0400

    Correct version number, add MANIFEST.in and modify setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,14 @@
 from setuptools import setup
 import os
-version = '0.3.1dev'
+version = '0.3.0dev'
 
-# Copy default config if not exists
-
-if not os.path.isdir('/home/pi/raspi-sump'):
-    cmds = ('mkdir /home/pi/raspi-sump/',
-            'cp conf/raspisump.conf /home/pi/raspi-sump/',
-            'mkdir /home/pi/raspi-sump/charts/',
-            'mkdir /home/pi/raspi-sump/logs/',
-            'mkdir /home/pi/raspi-sump/csv/',
-            'mkdir /home/pi/raspi-sump/docs/',
-            'cp docs/* /home/pi/raspi-sump/docs/',
-            'chown -R pi /home/pi/raspi-sump/'
-            )
-    for cmd in cmds:
-        os.system(cmd)
+if os.path.isdir('/home/pi/raspi-sump'):
+    cmd = 'chown -R pi /home/pi/raspi-sump/'
+    os.system(cmd)
+    cmd = 'chmod 700 /home/pi/raspi-sump/raspisump.conf'
+    os.system(cmd)
 else:
-    cmds = ('cp -u docs/* /home/pi/raspi-sump/docs',
-            'chown -R pi /home/pi/raspi-sump/'
-            )
-    for cmd in cmds:
-        os.system(cmd)
+    pass
 
 raspi_sump_files = ['raspisump/raspisump.py',
                     'raspisump/raspisump_alternate.py',
@@ -29,17 +16,32 @@ raspi_sump_files = ['raspisump/raspisump.py',
                     'raspisump/checkpid.py'
                     ]
 
-config = {
-    'name': 'Raspi-Sump',
-    'description': 'A sump pit monitoring system for Raspberry Pi',
-    'author': 'Al Audet',
-    'author_email': 'alaudet@linuxnorth.org.',
-    'url': 'http://www.linuxnorth.org/raspi-sump/',
-    'download_url': 'https://github.com/alaudet/raspi-sump/releases',
-    'version': version,
-    'license': 'MIT License',
-    'install_requires': ['RPi.GPIO'],
-    'scripts': raspi_sump_files
-}
+add_files = [('home/pi/raspi-sump', ['conf/raspisump.conf']),
+             ('home/pi/raspi-sump/docs', ['docs/README.md']),
+             ('home/pi/raspi-sump/docs', ['docs/automated_install.md']),
+             ('home/pi/raspi-sump/docs', ['docs/manual_install.md'])
+             ]
 
-setup(**config)
+setup(name='raspisump',
+      version=version,
+      description='A sump pit monitoring system for Raspberry Pi',
+      long_description=open("./README.md", "r").read(),
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Environment :: Console",
+          "Intended Audience :: End Users",
+          "Natural Language :: English",
+          "Operating System :: Raspbian Linux",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+          ],
+      author='Al Audet',
+      author_email='alaudet@linuxnorth.org',
+      url='http://www.linuxnorth.org/raspi-sump/',
+      download_url='https://github.com/alaudet/raspi-sump/releases',
+      license='MIT License',
+      scripts=raspi_sump_files,
+      data_files=add_files,
+      install_requires=['RPi.GPIO']
+      )
