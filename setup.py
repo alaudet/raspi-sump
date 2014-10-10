201cc88038c7a79c3131a956406253665b1b97d4
Al Audet <alaudet@linuxnorth.org>
2014-10-10 13:57:22 -0400

    First Beta to work with PIP

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 import os
-version = '0.3.0dev'
+version = '0.3.0beta1'
 
 raspi_sump_files = ['raspisump/raspisump.py',
                     'raspisump/raspisump_alternate.py',
@@ -15,7 +15,6 @@ add_files = [('/home/pi/raspi-sump', ['conf/raspisump.conf']),
              ('/home/pi/raspi-sump/docs', ['docs/README.md']),
              ('/home/pi/raspi-sump/docs', ['docs/automated_install.md']),
              ('/home/pi/raspi-sump/docs', ['docs/manual_install.md']),
-             ('/home/pi/raspi-sump', ['LICENSE']),
              ('/home/pi/raspi-sump/simulations', ['simulations/README.md']),
              ('/home/pi/raspi-sump/simulations',
                  ['simulations/sim-pump-fail.py']),
@@ -30,9 +29,9 @@ setup(name='raspisump',
       classifiers=[
           "Development Status :: 4 - Beta",
           "Environment :: Console",
-          "Intended Audience :: End Users",
+          "Intended Audience :: End Users/Desktop",
           "Natural Language :: English",
-          "Operating System :: Raspbian Linux",
+          "Operating System :: POSIX :: Linux",
           "Programming Language :: Python :: 2.7",
           "Topic :: Home Automation",
           "License :: OSI Approved :: MIT License",
@@ -42,6 +41,7 @@ setup(name='raspisump',
       url='http://www.linuxnorth.org/raspi-sump/',
       download_url='https://github.com/alaudet/raspi-sump/releases',
       license='MIT License',
+      packages=['raspisump'],
       scripts=raspi_sump_files,
       data_files=add_files,
       install_requires=['RPi.GPIO']
