80b71a2524b4048008c11057582934438d9c4072
Al Audet <alaudet@linuxnorth.org>
2019-07-06 08:04:47 -0400

    Changes to reading.py to address changes in hcsr04sensor

--- raspisump/reading.py
+++ raspisump/reading.py
@@ -1,4 +1,4 @@
-''' Module to take a water_level reading.'''
+""" Module to take a water_level reading."""
 
 # Raspi-sump, a sump pump monitoring system.
 # Al Audet
@@ -15,75 +15,78 @@ from hcsr04sensor import sensor
 from raspisump import log, alerts, heartbeat
 
 config = configparser.RawConfigParser()
-config.read('/home/pi/raspi-sump/raspisump.conf')
+config.read("/home/pi/raspi-sump/raspisump.conf")
 
-configs = {'critical_water_level': config.getint('pit', 'critical_water_level'),
-           'pit_depth': config.getint('pit', 'pit_depth'),
-           'temperature': config.getint('pit', 'temperature'),
-           'trig_pin': config.getint('gpio_pins', 'trig_pin'),
-           'echo_pin': config.getint('gpio_pins', 'echo_pin'),
-           'unit': config.get('pit', 'unit')
-           }
+configs = {
+    "critical_water_level": config.getint("pit", "critical_water_level"),
+    "pit_depth": config.getint("pit", "pit_depth"),
+    "temperature": config.getint("pit", "temperature"),
+    "trig_pin": config.getint("gpio_pins", "trig_pin"),
+    "echo_pin": config.getint("gpio_pins", "echo_pin"),
+    "unit": config.get("pit", "unit"),
+}
 
 # If item in raspisump.conf add to configs dict. If not provide defaults.
 try:
-    configs['alert_when'] = config.get('pit', 'alert_when')
+    configs["alert_when"] = config.get("pit", "alert_when")
 except configparser.NoOptionError:
-    configs['alert_when'] = 'high'
+    configs["alert_when"] = "high"
 
 try:
-    configs['heartbeat'] = config.getint('email', 'heartbeat')
+    configs["heartbeat"] = config.getint("email", "heartbeat")
 except configparser.NoOptionError:
-    configs['heartbeat'] = 0
+    configs["heartbeat"] = 0
 
 
 def initiate_heartbeat():
-    '''Initiate the heartbeat email process if needed'''
-    if configs['heartbeat'] == 1:
+    """Initiate the heartbeat email process if needed"""
+    if configs["heartbeat"] == 1:
         heartbeat.determine_if_heartbeat()
     else:
         pass
 
 
 def water_reading():
-    '''Initiate a water level reading.'''
-    pit_depth = configs['pit_depth']
-    trig_pin = configs['trig_pin']
-    echo_pin = configs['echo_pin']
-    round_to = 1
-    temperature = configs['temperature']
-    unit = configs['unit']
+    """Initiate a water level reading."""
+    pit_depth = configs["pit_depth"]
+    trig_pin = configs["trig_pin"]
+    echo_pin = configs["echo_pin"]
+    # round_to = 1
+    temperature = configs["temperature"]
+    unit = configs["unit"]
 
-    value = sensor.Measurement(trig_pin, echo_pin, temperature, unit, round_to)
-    
+    value = sensor.Measurement(trig_pin, echo_pin, temperature, unit)
+
     try:
         raw_distance = value.raw_distance(sample_wait=0.3)
     except SystemError:
-        log.log_errors("**ERROR - Signal not received. Possible cable or sensor problem.")
-        exit(0) 
-    
-    if unit == 'imperial':
-        return value.depth_imperial(raw_distance, pit_depth)
-    if unit == 'metric':
-        return value.depth_metric(raw_distance, pit_depth)
+        log.log_errors(
+            "**ERROR - Signal not received. Possible cable or sensor problem."
+        )
+        exit(0)
+
+    return round(value.depth(raw_distance, pit_depth), 1)
+    # if unit == "imperial":
+    #     return round(value.depth_imperial(raw_distance, pit_depth), 1)
+    # if unit == "metric":
+    #     return round(value.depth_metric(raw_distance, pit_depth), 1)
 
 
 def water_depth():
-    '''Determine the depth of the water, log result and generate alert
+    """Determine the depth of the water, log result and generate alert
     if needed.
-    '''
+    """
 
-    critical_water_level = configs['critical_water_level']
+    critical_water_level = configs["critical_water_level"]
     water_depth = water_reading()
-
 
     if water_depth < 0.0:
         water_depth = 0.0
     log.log_reading(water_depth)
-    
-    if water_depth > critical_water_level and configs['alert_when'] == 'high':
+
+    if water_depth > critical_water_level and configs["alert_when"] == "high":
         alerts.determine_if_alert(water_depth)
-    elif water_depth < critical_water_level and configs['alert_when'] == 'low':
+    elif water_depth < critical_water_level and configs["alert_when"] == "low":
         alerts.determine_if_alert(water_depth)
     else:
         pass
--- setup.py
+++ setup.py
@@ -1,65 +1,69 @@
 from setuptools import setup
 import os
-version = '1.2.1'
 
-homedir = '/home/pi/raspi-sump/'
+version = "1.2.3pre"
 
-if os.path.isfile(homedir + 'raspisump.conf'):
-    cmd = 'cp -u ' + homedir + 'raspisump.conf ' + homedir + \
-        'raspisump.conf.save'
+homedir = "/home/pi/raspi-sump/"
+
+if os.path.isfile(homedir + "raspisump.conf"):
+    cmd = "cp -u " + homedir + "raspisump.conf " + homedir + "raspisump.conf.save"
     os.system(cmd)
 
-raspi_sump_files = ['bin/rsump.py',
-                    'bin/rsumpchart.py',
-                    'bin/rsumpmonitor.py',
-                    'bin/rsumpwebchart.py'
-                    ]
+raspi_sump_files = [
+    "bin/rsump.py",
+    "bin/rsumpchart.py",
+    "bin/rsumpmonitor.py",
+    "bin/rsumpwebchart.py",
+]
 
-add_files = [(homedir + 'sample_config', ['conf/raspisump.conf']),
-             (homedir + 'sample_config', ['conf/lighttpd.conf']),
-             (homedir + 'csv', ['conf/csv/README.md']),
-             (homedir + 'logs', ['conf/logs/README.md']),
-             (homedir + 'charts', ['conf/charts/README.md']),
-             (homedir + 'docs', ['docs/README.md']),
-             (homedir + 'docs', ['docs/install.md']),
-             (homedir + 'cron', ['cron/README.md']),
-             (homedir + 'cron', ['cron/picrontab']),
-             (homedir + 'web', ['conf/web/index.html']),
-             (homedir + 'web/images', ['conf/web/images/logo.png']),
-             (homedir + 'web/css', ['conf/web/css/index.html']),
-             (homedir + 'web/css', ['conf/web/css/raspi.css'])
-             ]
+add_files = [
+    (homedir + "sample_config", ["conf/raspisump.conf"]),
+    (homedir + "sample_config", ["conf/lighttpd.conf"]),
+    (homedir + "csv", ["conf/csv/README.md"]),
+    (homedir + "logs", ["conf/logs/README.md"]),
+    (homedir + "charts", ["conf/charts/README.md"]),
+    (homedir + "docs", ["docs/README.md"]),
+    (homedir + "docs", ["docs/install.md"]),
+    (homedir + "cron", ["cron/README.md"]),
+    (homedir + "cron", ["cron/picrontab"]),
+    (homedir + "web", ["conf/web/index.html"]),
+    (homedir + "web/images", ["conf/web/images/logo.png"]),
+    (homedir + "web/css", ["conf/web/css/index.html"]),
+    (homedir + "web/css", ["conf/web/css/raspi.css"]),
+]
 
-setup(name='raspisump',
-      version=version,
-      description='A sump pit monitoring system for Raspberry Pi',
-      long_description=open("./README.md", "r").read(),
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Environment :: Console",
-          "Intended Audience :: End Users/Desktop",
-          "Natural Language :: English",
-          "Operating System :: POSIX :: Linux",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3.2",
-          "Programming Language :: Python :: 3.4",
-          "Topic :: Home Automation",
-          "License :: OSI Approved :: MIT License",
-          ],
-      author='Al Audet',
-      author_email='alaudet@linuxnorth.org',
-      url='http://www.linuxnorth.org/raspi-sump/',
-      download_url='https://github.com/alaudet/raspi-sump/releases',
-      license='MIT License',
-      packages=['raspisump'],
-      scripts=raspi_sump_files,
-      data_files=add_files,
-      install_requires=['hcsr04sensor']
-      )
+setup(
+    name="raspisump",
+    version=version,
+    description="A sump pit monitoring system for Raspberry Pi",
+    long_description=open("./README.md", "r").read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Home Automation",
+        "License :: OSI Approved :: MIT License",
+    ],
+    author="Al Audet",
+    author_email="alaudet@linuxnorth.org",
+    url="http://www.linuxnorth.org/raspi-sump/",
+    download_url="https://github.com/alaudet/raspi-sump/releases",
+    license="MIT License",
+    packages=["raspisump"],
+    scripts=raspi_sump_files,
+    data_files=add_files,
+    install_requires=["hcsr04sensor"],
+)
 
 if os.path.isdir(homedir):
-    cmd = 'chown -R pi ' + homedir
+    cmd = "chown -R pi " + homedir
     os.system(cmd)
-    cmd = 'chmod 600 ' + homedir + 'raspisump.conf'
+    cmd = "chmod 600 " + homedir + "raspisump.conf"
     os.system(cmd)
 
