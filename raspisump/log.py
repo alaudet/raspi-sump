09d58de4e88584646af721ba8c0b9ec63281ec19
usafpride <usaf.pride@gmail.com>
2019-08-05 16:11:41 -0400

    sqlite3 commit

--- raspisump/log.py
+++ raspisump/log.py
@@ -58,7 +58,7 @@ def log_heartbeat(notification):
     heartbeat_log.write("\n")
     heartbeat_log.close()
 
-def log_database(water_depth, database_name)
+def log_database(water_depth, database_name):
     '''Log time and water depth reading to sqlite3 database.'''
     write_database(water_depth, database_name)
 
