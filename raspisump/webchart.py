423aa170441c0ddaebeeb6fbc8c4e4146fdac38d
Al Audet <alaudet@linuxnorth.org>
2022-02-11 11:05:27 -0500

    fix webchart.py / remove deprecated bytes2str code

--- raspisump/webchart.py
+++ raspisump/webchart.py
@@ -1,4 +1,4 @@
-'''Create charts for viewing on Raspberry Pi Web Server.'''
+"""Create charts for viewing on Raspberry Pi Web Server."""
 
 # Raspi-sump, a sump pump monitoring system.
 # Al Audet
@@ -14,32 +14,29 @@ from raspisump import todaychart
 
 
 def create_folders(year, month, homedir):
-    '''Check if folders exist in charts folder and create them if they don't'''
-    if not os.path.isdir('{}charts/{}/'.format(homedir, year)):
-        _year = 'mkdir {}charts/{}'.format(homedir, year)
-        create_year = _year.split(' ')
+    """Check if folders exist in charts folder and create them if they don't"""
+    if not os.path.isdir("{}charts/{}/".format(homedir, year)):
+        _year = "mkdir {}charts/{}".format(homedir, year)
+        create_year = _year.split(" ")
         subprocess.call(create_year)
 
-    if not os.path.isdir('{}charts/{}/{}/'.format(homedir, year, month)):
-        _month = 'mkdir {}charts/{}/{}'.format(homedir, year, month)
-        create_month = _month.split(' ')
+    if not os.path.isdir("{}charts/{}/{}/".format(homedir, year, month)):
+        _month = "mkdir {}charts/{}/{}".format(homedir, year, month)
+        create_month = _month.split(" ")
         subprocess.call(create_month)
 
 
 def create_chart(homedir):
-    '''Create a chart of sump pit activity and save to web folder'''
-    csv_file = '{}csv/waterlevel-{}.csv'.format(
-        homedir, time.strftime('%Y%m%d')
-        )
-    filename = '{}charts/today.png'.format(homedir)
-    bytes2str = todaychart.bytesdate2str('%H:%M:%S')
-    todaychart.graph(csv_file, filename, bytes2str)
+    """Create a chart of sump pit activity and save to web folder"""
+    csv_file = "{}csv/waterlevel-{}.csv".format(homedir, time.strftime("%Y%m%d"))
+    filename = "{}charts/today.png".format(homedir)
+    todaychart.graph(csv_file, filename)
 
 
 def copy_chart(year, month, today, homedir):
-    '''Copy today.png to year/month/day folder for web viewing'''
-    copy_cmd = 'cp {}charts/today.png {}charts/{}/{}/{}.png'.format(
+    """Copy today.png to year/month/day folder for web viewing"""
+    copy_cmd = "cp {}charts/today.png {}charts/{}/{}/{}.png".format(
         homedir, homedir, year, month, today
-        )
-    copy_file = copy_cmd.split(' ')
+    )
+    copy_file = copy_cmd.split(" ")
     subprocess.call(copy_file)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import os
 
-version = "1.2.4rc2"
+version = "1.2.4rc3"
 
 homedir = "/home/pi/raspi-sump/"
 
